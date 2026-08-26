f00b72905fb3d9b412b726fd12916b347e1730a6
UserLEmanuel <lepadatuemanuel7@gmail.com>
2026-08-26 23:18:57 +0300

    new parser with new lib

--- src/parser.py
+++ src/parser.py
@@ -1,53 +1,38 @@
+import cyvcf2
 from models import Variant
-
 
 def parse_vcf(file_path):
     variants = []
 
-    with open(file_path, "r") as file:
-        for line in file:
+    for record in cyvcf2.VCF(file_path):
+        chromosome = record.CHROM
+        position = record.POS
+        reference = record.REF
+        alternative = record.ALT[0]  # primul ALT, dacă sunt mai multe
+        quality = record.QUAL if record.QUAL is not None else 0.0
+        filter_status = record.FILTER if record.FILTER is not None else "PASS"
 
-            if line.startswith("#"):
-                continue
+        variant_type = classify_variant(reference, alternative)
 
-            columns = line.strip().split()
-
-            chromosome = columns[0]
-            position = int(columns[1])
-            reference = columns[3]
-            alternative = columns[4]
-            quality = float(columns[5])
-            filter_status = columns[6]
-
-            variant_type = classify_variant(
-                reference,
-                alternative
-            )
-
-            variant = Variant(
-                chromosome=chromosome,
-                position=position,
-                reference=reference,
-                alternative=alternative,
-                quality=quality,
-                filter=filter_status,
-                type=variant_type
-            )
-
-            variants.append(variant)
+        variant = Variant(
+            chromosome=chromosome,
+            position=position,
+            reference=reference,
+            alternative=alternative,
+            quality=quality,
+            filter=filter_status,
+            type=variant_type
+        )
+        variants.append(variant)
 
     return variants
 
 
 def classify_variant(reference, alternative):
-
     if len(reference) == 1 and len(alternative) == 1:
         return "SNP"
-
     if len(reference) < len(alternative):
         return "Insertion"
-
     if len(reference) > len(alternative):
         return "Deletion"
-
     return "Other"
